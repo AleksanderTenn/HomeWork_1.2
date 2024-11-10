b5961d87128f4c3278f052a14b80b66d57fb6be2
AleksanderTenn <naydan03081977@gmail.com>
2024-11-10 20:44:59 +0300

    Переменные

---
+++ main.py
@@ -0,0 +1,5 @@
+Home_work_count=12
+Home_work_time=1.5
+Name_kurs='Python'
+Time_one=Home_work_time/Home_work_count
+print(Name_kurs,', всего задач: ',Home_work_count,', затрачено часов: ',Home_work_time,', среднее время выполнения: ',Time_one)
